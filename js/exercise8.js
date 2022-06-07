c66a781035fc3e5d71fb59c82c3d23b857bdbb8d
Mariano Grosso <mariano1092@gmail.com>
2022-06-07 18:15:59 -0300

    1: Detección de Dispositivo

--- js/exercise8.js
+++ js/exercise8.js
@@ -1,26 +1,30 @@
 const doc = document
 
-export default function responsiveTester(webpage, width, height, submit, submitContainer) {
+export default function responsiveTester(webpage, width, height, form) {
+  doc.addEventListener('submit', e => {
+    e.preventDefault()
+    let webpageContent = doc.getElementById(webpage).value
+    let widthContent = doc.getElementById(width).value
+    let heightContent = doc.getElementById(height).value
+    let cerrar = doc.getElementById(form).cerrar
 
-  doc.addEventListener("click", e => {
-    if (e.target.matches(submit)) {
-      e.preventDefault()
-      let webpageContent = doc.getElementById(webpage).value
-      let widthContent = doc.getElementById(width).value
-      let heightContent = doc.getElementById(height).value
-
-      let windowOpen = window.open(webpageContent, "_blank", ` toolbar=no,
+    let windowOpen = window.open(
+      webpageContent,
+      '_blank',
+      ` toolbar=no,
       location=no,
       status=no,
       menubar=no,
       scrollbars=yes,
       resizable=no,
       height=${heightContent},
-      width=${widthContent}`)
-    }
+      width=${widthContent}`
+    )
 
-    if (e.target.matches("#close-btn")) {
-      window.close(windowOpen)
-    }
+    doc.addEventListener('click', e => {
+      if (e.target === cerrar) {
+        windowOpen.close()
+      }
+    })
   })
-} 
+}
--- js/index.js
+++ js/index.js
@@ -1,30 +1,39 @@
-import handleHamburger from './exercise1.js';
-import { showClock, initAlarm } from './exercise2.js';
-import { moveBall } from './exercise3.js';
-import { countdown } from './exercise4.js';
-import moveScroll from './exercise5.js';
-import toggleTheme from './exercise6.js';
-import responsiveMedia from './exercise7.js';
-import responsiveTester from './exercise8.js';
+import handleHamburger from './exercise1.js'
+import { showClock, initAlarm } from './exercise2.js'
+import { moveBall } from './exercise3.js'
+import { countdown } from './exercise4.js'
+import moveScroll from './exercise5.js'
+import toggleTheme from './exercise6.js'
+import responsiveMedia from './exercise7.js'
+import responsiveTester from './exercise8.js'
 
-const doc = document;
+const doc = document
 
-doc.addEventListener('DOMContentLoaded', (e) => {
-	handleHamburger('.burger-menu', '.aside', '.navbar a');
-	showClock('.clock-content', '.init-clock', '.stop-clock');
-	initAlarm('assets/alarm.mp3', '.init-alarm', '.stop-alarm');
-	countdown('.countdown-container', 'Sep 10, 2022 10:23:43');
-	moveScroll('.up-scroll', '.aside');
-	responsiveMedia("youtube-link", "(min-width: 1024px)", `<a href="https://www.youtube.com/embed/eVTXPUF4Oz4" target="_blank">Link YouTube</a>`, `
+doc.addEventListener('DOMContentLoaded', e => {
+  handleHamburger('.burger-menu', '.aside', '.navbar a')
+  showClock('.clock-content', '.init-clock', '.stop-clock')
+  initAlarm('assets/alarm.mp3', '.init-alarm', '.stop-alarm')
+  countdown('.countdown-container', 'Sep 10, 2022 10:23:43')
+  moveScroll('.up-scroll', '.aside')
+  responsiveMedia(
+    'youtube-link',
+    '(min-width: 1024px)',
+    `<a href="https://www.youtube.com/embed/eVTXPUF4Oz4" target="_blank">Link YouTube</a>`,
+    `
 	<iframe width="560" height="315" src="https://www.youtube.com/embed/eVTXPUF4Oz4"  title="YouTube video player" frameborder="0" allow="accelerometer; autoplay;   clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
-	)
-	responsiveMedia("gmap-link", "(min-width: 1024px)", `<a href="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d53629.092338119495!2d-68.87290759185294!3d-32.850063034522364!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x967e08972bd84163%3A0xc69f774a11aa269a!2sIES%209008%20Manuel%20Belgrano!5e0!3m2!1ses-419!2sar!4v1653094234752!5m2!1ses-419!2sar" target="_blank">Link Google Map</a>`, `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d53629.092338119495!2d-68.87290759185294!3d-32.850063034522364!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x967e08972bd84163%3A0xc69f774a11aa269a!2sIES%209008%20Manuel%20Belgrano!5e0!3m2!1ses-419!2sar!4v1653094234752!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-	`)
-	responsiveTester("webpage", "width-page", "height-page", "#open-btn", ".submit-btns")
-});
+  )
+  responsiveMedia(
+    'gmap-link',
+    '(min-width: 1024px)',
+    `<a href="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d53629.092338119495!2d-68.87290759185294!3d-32.850063034522364!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x967e08972bd84163%3A0xc69f774a11aa269a!2sIES%209008%20Manuel%20Belgrano!5e0!3m2!1ses-419!2sar!4v1653094234752!5m2!1ses-419!2sar" target="_blank">Link Google Map</a>`,
+    `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d53629.092338119495!2d-68.87290759185294!3d-32.850063034522364!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x967e08972bd84163%3A0xc69f774a11aa269a!2sIES%209008%20Manuel%20Belgrano!5e0!3m2!1ses-419!2sar!4v1653094234752!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+	`
+  )
+  responsiveTester('webpage', 'width-page', 'height-page', 'section-five__form')
+})
 
-doc.addEventListener('keydown', (e) => {
-	moveBall(e, '.game-stage', '.game-ball');
-});
+doc.addEventListener('keydown', e => {
+  moveBall(e, '.game-stage', '.game-ball')
+})
 
-toggleTheme(".theme-box", "dark-theme")
+toggleTheme('.theme-box', 'dark-theme')
