8c5cbbe63052045e21f263d5f76a1b361bbf9113
Mariano Grosso <mariano1092@gmail.com>
2022-07-07 21:55:25 -0300

    Fetch API

--- ajax/ajax.js
+++ ajax/ajax.js
@@ -8,7 +8,6 @@
     if (xhr.readyState !== 4) return
     if (xhr.status >= 200 && xhr.status < 300) {
       let responseTextJSON = JSON.parse(xhr.responseText)
-      console.log(responseTextJSON)
 
       responseTextJSON.forEach(element => {
         const $li = document.createElement("li")
@@ -25,4 +24,62 @@
   xhr.open("GET", "https://jsonplaceholder.typicode.com/users")
   xhr.send()
 
-})()
+})();
+
+(() => {
+  const $fetch = document.getElementById("fetch")
+  const $fragment = document.createDocumentFragment()
+
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => response.ok ? response.json() : Promise.reject(response))
+    .then(responseJson => {
+      responseJson.forEach(element => {
+        const $li = document.createElement("li")
+        $li.innerHTML = `Nombre: ${element.name} -- Email: ${element.email}`
+        $fragment.appendChild($li)
+        $fetch.appendChild($fragment)
+      })
+    }
+
+    )
+    .catch(err => {
+      let $message = document.createElement("p")
+      $message.textContent = `Error ${err.status}. Ocurrió un error`
+
+      $fetch.appendChild($message)
+    })
+    .finally(() => {
+      console.log("Se ejecuta de todas maneras.")
+    })
+})();
+
+(() => {
+  const $fetchAA = document.getElementById("fetch-aa")
+  const $fragment = document.createDocumentFragment()
+
+  async function getData() {
+    try {
+      let response = await fetch("https://jsonplaceholder.typicode.com/users")
+      let responseJson = await response.json()
+
+      if (!response.ok) throw { status: response.status, statusText: response.statusText }
+
+      responseJson.forEach(element => {
+        const $li = document.createElement("li")
+        $li.innerHTML = `Nombre: ${element.name} -- Email: ${element.email}`
+        $fragment.appendChild($li)
+      })
+
+      $fetchAA.appendChild($fragment)
+    } catch (error) {
+      let $message = document.createElement("p")
+      $message.textContent = `Error ${error.status}. Ocurrió un error`
+
+      $fetchAA.appendChild($message)
+    } finally {
+      console.log("Se ejecuta de todas formas 2")
+    }
+  }
+
+  getData()
+})();
